d3b4ec22849005588f775037a064e5f60d911480
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-10-07 15:22:00 +0200

    remove built dir

--- packages/gql-server/built/src/generated/graphql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.DataSourceType = void 0;
-var DataSourceType;
-(function (DataSourceType) {
-    DataSourceType["Ccdb"] = "CCDB";
-    DataSourceType["Dplsink"] = "DPLSINK";
-})(DataSourceType = exports.DataSourceType || (exports.DataSourceType = {}));
